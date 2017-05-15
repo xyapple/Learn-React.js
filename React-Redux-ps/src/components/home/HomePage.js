a56a108e5f4135070b10f5b631bdc867f223f8a0
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-05-15 13:57:46 -0400

    Add About and Home Pages javascript files

---
+++ React-Redux-ps/src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {link} from 'react-router';
+
+class HomePage extends React.Component{
+  render() {
+    return (
+      <div className = "jumbotron">
+        <h1>PS Administration</h1>
+      <p>Learn REACT-REDUX and ES6 ect</p>
+      </div>
+    )
+
+  }
+}
