1856a5f8c32c3b4a17a9803a3e136dcda4d4ab2d
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-05-15 14:35:14 -0400

    Add the About Page

---
+++ React-Redux-ps/src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class AboutPage {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+      <p>This application uses React-Redux, React-Router </p>
+      </div>
+    )
+
+  }
+}
+export default AboutPage;
--- React-Redux-ps/src/components/home/HomePage.js
+++ React-Redux-ps/src/components/home/HomePage.js
@@ -7,8 +7,10 @@ class HomePage extends React.Component{
       <div className = "jumbotron">
         <h1>PS Administration</h1>
       <p>Learn REACT-REDUX and ES6 ect</p>
+    <link to="about" className="btn btn-primary btn-lg">Learn!React</link>
       </div>
     )
 
   }
 }
+export default HomePage;
