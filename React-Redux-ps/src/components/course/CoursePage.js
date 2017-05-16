c10ac89d48335660d49c879c509a940aa51d8e70
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-05-16 11:12:09 -0400

    Update the CoursePage with onTitleChange and onClickSave functions; Then Bind them inside the constructor with this.'function'.bind(this).

--- React-Redux-ps/src/components/course/CoursePage.js
+++ React-Redux-ps/src/components/course/CoursePage.js
@@ -1,13 +1,39 @@
 import React, {PropTypes} from 'react';
 
 class CoursePage extends React.Component {
+  constructor(props, context){
+    super(props, context);
+    this.state={
+      course:{title: ""}
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+onTitleChange(event){
+  const course = this.state.course;
+  course.title=event.target.value;
+  this.setState({course:course});
+}
+onClickSave(event){
+  alert(`Saving ${this.state.course.title}`);
+}
   render() {
     return(
+      <div>
         < div >
           <h1>Courses</h1>
-        < /div>
+          <h2>Add Courses</h2>
+          <input type="text"
+            onChange={this.onTitleChange}
+          value={this.state.course.title}/>
 
+          <input type="submit"
+          value="Save"
+        onClick={this.onClickSave}/>
+        < /div>
+     </div>
     );
   }
 }
+
 export default CoursePage;
